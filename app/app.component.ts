856bb76b9ed3910ff7e4dbb85ff61aae8ec4cff1
pietruh <pietruh@MacBook-Pro-pietruh.local>
2016-11-30 00:18:50 +0100

    modification add leaflet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { L } from 'leaflet';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+          <div id="map" style="width:100%, height:100%"></div>
+  `
+
+  ,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+    name = 'Angular';
+  private map: any;
+  ngOnInit() {
+
+    this.map = L.map("map").setView([23.709921, 90.407143], 7);
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '©OpenStreetMap, ©CartoDB'
+}).addTo(this.map);
+
+  }
+}
--- systemjs.config.js
+++ systemjs.config.js
@@ -28,7 +28,9 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      leaflet:                    'npm:leaflet/dist/leaflet-src.js'
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
