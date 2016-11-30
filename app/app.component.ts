134b3f22f471806f3c741393ed09a9d4e9d88ab6
pietruh <pietruh@MacBook-Pro-pietruh.local>
2016-12-01 00:22:55 +0100

    version changes, modification

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { L } from 'leaflet';
+import 'leaflet';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-          <div id="map" style="width:100%, height:100%"></div>
+  template: `
+          <div id="map" style="width:100%; height:1000px"></div>
   `
-
-  ,
 })
 export class AppComponent implements OnInit {
-    name = 'Angular';
+  name = 'Angular';
   private map: any;
   ngOnInit() {
 
-    this.map = L.map("map").setView([23.709921, 90.407143], 7);
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB'
-}).addTo(this.map);
+    this.map = L.map("map", {
+      center: [49.749921, 21.4207143],
+      zoom: 13
+    });
+    // setView([23.709921, 90.407143], 7);
+    
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+  
 
   }
 }
